e60b1afb619afca6b814dbe21f9ba7a85ba2451a
Bento <joaopbento05@gmail.com>
2026-03-25 12:22:16 -0100

    First commit

--- src/main/java/net/airlines/tutorialmod/TutorialMod.java
+++ src/main/java/net/airlines/tutorialmod/TutorialMod.java
@@ -15,6 +15,8 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+//This comment is the first commit test
+
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod {
